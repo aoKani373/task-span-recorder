5ab79e9236f4ca7b2a34fc0af00214055eaac024
aoKani373 <ask_o373@outlook.com>
2026-03-25 21:19:00 +0900

    Add Timeline Control

--- TaskSpanRecorder/Models/TaskSpan.cs
+++ TaskSpanRecorder/Models/TaskSpan.cs
@@ -10,16 +10,22 @@ namespace TaskSpanRecorder.Models
     public partial class TaskSpan : ObservableObject
     {
         public int Id { get; set; }
-
         public int TaskCategoryId { get; set; }
-
         public DateOnly Date { get; set; }
-
         public TimeOnly StartTime { get; set; }
 
         [ObservableProperty]
         private TimeOnly? _endTime = null;
 
         public TaskCategory? TaskCategory { get; set; } = null;
+
+        public double StartSeconds => StartTime.ToTimeSpan().TotalSeconds;
+
+        public double DurationSeconds => ((EndTime ?? TimeOnly.FromDateTime(DateTime.Now)) - StartTime).TotalSeconds;
+
+        partial void OnEndTimeChanged(TimeOnly? value)
+        {
+            OnPropertyChanged(nameof(DurationSeconds));
+        }
     }
 }
